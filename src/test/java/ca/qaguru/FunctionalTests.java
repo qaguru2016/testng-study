bb8deacd96672632d83a10e15b91be95bdd258f7
pradeep.pp <pradeep.pp@gmail.com>
2024-04-23 16:29:34 -0400

    Project and sample tests created

---
+++ src/test/java/ca/qaguru/FunctionalTests.java
@@ -0,0 +1,54 @@
+package ca.qaguru;
+
+import org.testng.annotations.*;
+
+public class FunctionalTests {
+    @BeforeSuite
+    public void beforeSuite(){
+        System.out.println("BeforeSuite in FunctionalTests");
+    }
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("AfterSuite in FunctionalTests");
+    }
+
+    @BeforeTest
+    public void beforeTest(){
+        System.out.println("BeforeTest in FunctionalTests");
+    }
+    @AfterTest
+    public void afterTest(){
+        System.out.println("AfterTest in FunctionalTests");
+    }
+
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("BeforeClass in FunctionalTests");
+    }
+    @AfterClass
+    public void afterClass(){
+        System.out.println("AfterClass in FunctionalTests");
+    }
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("BeforeMethod in FunctionalTests");
+    }
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("AfterMethod in FunctionalTests");
+    }
+
+    @Test
+    public void funcTest1(){
+        System.out.println("funcTest1");
+    }
+    @Test
+    public void funcTest2(){
+        System.out.println("funcTest2");
+    }
+    @Test
+    public void funcTest3(){
+        System.out.println("funcTest3");
+    }
+
+}
---
+++ src/test/java/ca/qaguru/SecurityTests.java
@@ -0,0 +1,36 @@
+package ca.qaguru;
+
+import org.testng.annotations.*;
+
+public class SecurityTests {
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("BeforeMethod in SecurityTests");
+    }
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("AfterMethod in SecurityTests");
+    }
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("BeforeClass in SecurityTests");
+    }
+    @AfterClass
+    public void afterClass(){
+        System.out.println("AfterClass in SecurityTests");
+    }
+    @Test
+    public void secTest1(){
+        System.out.println("secTest1");
+    }
+
+    @Test
+    public void secTest2(){
+        System.out.println("secTest2");
+    }
+    @Test
+    public void secTest3(){
+        System.out.println("secTest3");
+    }
+
+}
